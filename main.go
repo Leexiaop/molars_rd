a2e0b3310e746e8280aebfe82d9df7cc8babd6af
李晓鹏 <lixiaoeng@zstax.com>
2024-01-12 10:05:39 +0800

    热更新

--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"syscall"
+
+	"github.com/fvbock/endless"
 
 	"github.com/Leexiaop/molars_rd/pkg/setting"
 	"github.com/Leexiaop/molars_rd/routers"
 )
 func main () {
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	server := endless.NewServer(endPoint, routers.InitRouter())
 
-	s := &http.Server {
-		Addr: fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler: router,
-		ReadTimeout: setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server.BeforeBegin = func (add string)  {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+    if err != nil {
+        log.Printf("Server err: %v", err)
+    }
 }
